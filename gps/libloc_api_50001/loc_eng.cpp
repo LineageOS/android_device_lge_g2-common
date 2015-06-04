f72ebe7ce159e50ea1f76b4a092f5af714fa07d0
Christopher R. Palmer <crpalmer@gmail.com>
2015-06-04 15:45:55 -0400

    g2-common: gps: Unlock the GPS
    [haggertk]:
     * During bring-up of O, GPS was exhibiting different behavior from
       N -- an appropriate number of satellites would immediately lock,
       but a fix would not resolve for 30-90 seconds. Digging up this
       change from the L-era allows GPS to resolve a fix in a nominal
       amount of time.
    
    Change-Id: If9fd67483fedee915d46f7b9d2a7e0851400de3c

--- gps/libloc_api_50001/loc_eng.cpp
+++ gps/libloc_api_50001/loc_eng.cpp
@@ -1403,6 +1403,9 @@ struct LocEngInit : public LocMsg {
     }
     inline virtual void proc() const {
         loc_eng_reinit(*mLocEng);
+        mLocEng->adapter->setGpsLock(1);
+        // set the capabilities
+        mLocEng->adapter->sendMsg(new LocEngSetCapabilities(mLocEng));
     }
     inline void locallog() const
     {
