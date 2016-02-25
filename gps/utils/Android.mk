c423692a10b60fd61dadd20ec3921c3d22a2e1c8
Aurimas Liutikas <aurimas@google.com>
2016-02-24 17:40:49 -0800

    g2-common: gps: Suppress unused-parameter warnings
    Bug: 26936282
    Change-Id: I7fdf58d232a59c0ba0d5f52c0f958f6eab0c4514

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -22,7 +22,8 @@ LOCAL_SRC_FILES += \
 
 LOCAL_CFLAGS += \
      -fno-short-enums \
-     -D_ANDROID_
+     -D_ANDROID_ \
+     -Wno-unused-parameter
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
    LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
