8fb1ab5d627eade268eb8cb4ee122cd5d7386e91
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-09-09 08:30:44 -0500

    g2-common: move to source-built libril
    * Remove the shim, we dont need that shit anymore
    * Kangbang the f out of bacon's qmi stack
    * Remove the custom ril class, this is no longer necessary
    * Use local libril module (version 10)
    * Rework radio properties
    * Build librmnetctl
    
    Change-Id: I7e27a455d369abd69bc1d350e178a1e012411f05

--- g2.mk
+++ g2.mk
@@ -224,6 +224,7 @@ PRODUCT_PACKAGES += \
 # Qualcomm
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
+    librmnetctl \
     libxml2
 
 # Recovery
@@ -233,10 +234,6 @@ PRODUCT_PACKAGES += \
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
-
-# RIL
-PRODUCT_PACKAGES += \
-    libril_shim
 
 # Seccomp
 PRODUCT_COPY_FILES += \
