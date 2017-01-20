628ca716362485441e412de84a3dfc2acf978ed3
Nikola Majkić <n1kolaa924@gmail.com>
2017-01-20 20:48:23 +0000

    g2-common: Baseband fix 2
    ops, need to test it.
    
    Change-Id: I36b9304cdc072caa5cab46f7f8f906f9117c5f8f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := set_baseband.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/set_baseband.sh
+LOCAL_SRC_FILES         := bin/set_baseband.sh
 include $(BUILD_PREBUILT)
 
 # Init scripts
