20cd677a20eeb41a305766bfdf5082721f94a9c4
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-15 10:41:18 -0700

    g2-common: Move uevent.qcom.rc to /vendor
    Change-Id: Iab2699bf26ca4402b835d1af8d4493f9b28595d9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,7 +24,7 @@ LOCAL_MODULE            := init.g2.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -32,7 +32,7 @@ LOCAL_MODULE            := init.g2.power.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.power.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -40,7 +40,7 @@ LOCAL_MODULE            := init.g2.usb.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.usb.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -56,5 +56,5 @@ LOCAL_MODULE            := ueventd.g2.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/ueventd.g2.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
