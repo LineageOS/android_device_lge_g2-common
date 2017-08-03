eddfd51d49029193b536f0655e13ca090851db60
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-03 21:53:21 +0200

    g2: Move common ril class and ril shim from g2-common
    * Only cdma devices (vs980 and ls980) will need LG ril, so move it down to the variants
    * Based on unmerged g3 change and g2 changes https://review.lineageos.org/#/c/19327/
    
    Change-Id: I7b1140569e605c7bdc0eb04f27fbd78d59326f59

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,9 +113,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/g2-common/releasetools
 
-# RIL
-BOARD_RIL_CLASS += ../../../device/lge/g2-common/ril
-
 # SDClang
 TARGET_USE_SDCLANG := true
 
