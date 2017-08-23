ff436cd12e813088e286b7736c5cd9e923316dd6
Simon Shields <simon@lineageos.org>
2017-08-23 17:23:00 +0300

    camerawrapper: add missing libs for headers
    Change-Id: Ifdaf82855e9ccc3b9d8e13f4124e3670b803109d

--- camera/Android.mk
+++ camera/Android.mk
@@ -4,8 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_C_INCLUDES += \
     system/media/camera/include
