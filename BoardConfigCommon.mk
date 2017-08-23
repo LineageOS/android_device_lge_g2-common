00708a1ff7fa9f32371709c8a5a62c914cbfdf99
Simon Shields <simon@lineageos.org>
2017-08-23 17:30:00 +0300

    DO NOT MERGE: g2: nuke sepolicy, boot permissive
    Change-Id: I68daecdc3a780dc597cb28c89efc2ea73c54acc2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,7 @@ TARGET_NO_RADIOIMAGE := true
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2-common/releasetools/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -127,10 +127,10 @@ BOARD_RIL_CLASS += ../../../device/lge/g2-common/ril
 TARGET_USE_SDCLANG := true
 
 # SELinux policies
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    device/lge/g2-common/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    device/lge/g2-common/sepolicy
 
 # Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
