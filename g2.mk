4ce56f03139d6fb8ac7811e09ae9a1fe7bf8f5d2
Ethan Chen <intervigil@gmail.com>
2017-09-17 02:27:36 -0700

    g2-common: Convert lights HAL to native binderized HAL
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

--- g2.mk
+++ g2.mk
@@ -169,9 +169,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8974
+    android.hardware.light@2.0-service.g2
 
 # OMX
 PRODUCT_PACKAGES += \
