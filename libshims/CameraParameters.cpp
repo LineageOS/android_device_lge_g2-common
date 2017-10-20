0c957633bd1398cb02c9550f32804d5546d3a64b
jrior001 <jriordan001@gmail.com>
2017-10-20 06:00:00 +0300

    g2-common: convert CameraParameters header to shim lib
    Change-Id: If02fcb506da12ab4b8a0996d97da970db8c9c43c

---
+++ libshims/CameraParameters.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "CameraParameters.h"
+
+namespace android {
+const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot";
+const char CameraParameters::BURST_SHOT_ON[] = "burst-shot";
+const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual-position";
+const char CameraParameters::KEY_BURST_SHOT[] = "burst-shot";
+const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING[] = "object-tracking";
+const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "white-balance-manual";
+}; // namespace android
