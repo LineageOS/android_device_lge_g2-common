3cf46bf001c53078aa7e521adc172bc8e3f178f8
jrior001 <jriordan001@gmail.com>
2017-10-20 06:00:00 +0300

    g2-common: convert CameraParameters header to shim lib
    Change-Id: If02fcb506da12ab4b8a0996d97da970db8c9c43c

--- g2.mk
+++ g2.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl-legacy \
     Snap \
     camera.msm8974 \
-    libshim_atomic
+    libshim_atomic \
+    libshim_camera_parameters
 
 # Display
 PRODUCT_PACKAGES += \
