6ca755bed54bee4d36b24b063f078223dcbdd3c1
KhabarlakKonstantin <habarlack@gmail.com>
2017-12-24 16:25:11 +0200

    device/manifest: Use binderized NFC HAL
    It seems that this fixes NFC
    
    Change-Id: Ife23bacfc317c5a8a13972aa6ae628ad58cf366b

--- g2.mk
+++ g2.mk
@@ -186,7 +186,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service \
     Tag \
     com.android.nfc_extras
 
