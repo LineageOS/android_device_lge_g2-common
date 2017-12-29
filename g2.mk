c9bea02560213fd7a2583d684c68d36e68e4b0dd
Bruno Martins <bgcngm@gmail.com>
2017-12-29 22:58:39 +0000

    g2-common: Ship RenderScript HAL
    Change-Id: Ia13a26c46c803f6853b1188449547c70d3305bfa

--- g2.mk
+++ g2.mk
@@ -230,6 +230,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_g2
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     libril_shim
