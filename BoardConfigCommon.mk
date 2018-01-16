42579da9c496c16c2ec0a7d53f594156c92a4ef6
KhabarlakKonstantin <habarlack@gmail.com>
2018-01-16 18:34:34 +0200

    [DNM]: Boot permissive
    Change-Id: Ia79439cf2847a46ca4aca07bcc774b76ae62d180

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,7 @@ TARGET_NO_RADIOIMAGE := true
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2-common/releasetools/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
