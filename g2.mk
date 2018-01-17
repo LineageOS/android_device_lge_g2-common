921279665026534ce5f773c1c05ad52229465040
KhabarlakKonstantin <habarlack@gmail.com>
2018-01-17 19:04:12 +0200

    g2: Remove deprecated audiod service
    Change-Id: I07e8e4b8ef90c9102e1a3182cce5bc7599125868

--- g2.mk
+++ g2.mk
@@ -71,7 +71,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
-    audiod \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
