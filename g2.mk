052947c6ba4e92add1ae14ce2da9809721c1005e
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 19:21:50 +0200

    g2-common: Move thermal-engine-8974.conf to vendor
    * Requires following hex edit to load config from vendor:
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine
    
    Change-Id: I4d66abd270551a94a74c6017516ce489d803d0ba

--- g2.mk
+++ g2.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/vendor/etc/thermal-engine-8974.conf
 
 # USB
 PRODUCT_PACKAGES += \
