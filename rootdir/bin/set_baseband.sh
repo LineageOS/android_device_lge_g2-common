264541c0955370ca6fdc95dfcf9eecc9e66494cd
Arnau <arnaullv@gmail.com>
2018-01-29 22:15:23 +0100

    g2-common: Fix set_baseband.sh broken due to missing toybox symlinks
    Our set_baseband.sh script was broken after the introduction of
    https://review.lineageos.org/#/c/203257/
    
    Users report "Unknown baseband" on about dialog
    
    Change-Id: I86240257115f5752cb747db4c2b702b25b572908

--- rootdir/bin/set_baseband.sh
+++ rootdir/bin/set_baseband.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 
 # grep the modem partition for baseband version and set it
-if strings /firmware/image/modem.b21 | grep "^M8974A-" >> /dev/null; then
+if strings /firmware/image/modem.b21 | toybox grep "^M8974A-" >> /dev/null; then
     # "d800" | "d801" | "d802" | "d803" | "f320" | "l01f"
-    setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
-elif strings /firmware/image/modem.b18 | grep "^MPSS.DI" >> /dev/null; then
+    setprop gsm.version.baseband `strings /firmware/image/modem.b21 | toybox grep "^M8974A-" | head -1`
+elif strings /firmware/image/modem.b18 | toybox grep "^MPSS.DI" >> /dev/null; then
     # "ls980" | "vs980"
-    setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
+    setprop gsm.version.baseband `strings /firmware/image/modem.b18 | toybox grep "^MPSS.DI" | head -1`
 fi
