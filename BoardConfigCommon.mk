a2c3380251a00fbd367529e96ac5f046a324056a
Arnau <arnaullv@gmail.com>
2018-02-10 14:11:59 +0100

    g2-common: Move bcmdhd wireless firmware files to vendor
    Change-Id: I75a9241f811675da13fcd0334462437c13fc36e7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,5 +158,5 @@ BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
