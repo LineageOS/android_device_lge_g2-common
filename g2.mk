e11d601a1429b846dfd75b2acc409f633cd018f3
YoDevil <alessandro.astone@hotmail.com>
2018-04-15 12:00:04 +0200

    g2-common: camera: add missing camera provider service
    Change-Id: I10582643d91d1f12bccc3db588701d66bb549cd3

--- g2.mk
+++ g2.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl-legacy \
     Snap \
     camera.msm8974 \
