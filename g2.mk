ee98a291c1fce5aea9fc1ed929d5ff3fbc36f88e
Arnau <arnaullv@gmail.com>
2018-06-14 01:16:40 +0200

    g2-common: Change Keymaster HIDL to passthrough and remove the service
    This fixes lockscreen pin problems
    
    Change-Id: Ida14bbc58849e79e61c552a08ca157553968f3b9

--- g2.mk
+++ g2.mk
@@ -169,8 +169,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl
 
 # Lights
 PRODUCT_PACKAGES += \
