8fb204e8f179fe98ed8f16918cc3815c01d5f36a
Alessandro Astone <alessandro.astone@hotmail.com>
2018-08-07 21:25:27 +0200

    g2-common: recovery: updates for Pie
    Change-Id: I480386b8ecbb9e308e047cc743b4da087e9b44a3

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include
+LOCAL_C_INCLUDES := \
+    bootable/recovery/updater/include \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_g2
 LOCAL_MODULE_TAGS := eng
