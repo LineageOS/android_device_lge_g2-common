3001d1cbcb1ea767e7fa8d9ae2456c11ef60d0c6
Alessandro Astone <alessandro.astone@hotmail.com>
2018-08-07 21:25:27 +0200

    recovery: updates for Pie
    Change-Id: I480386b8ecbb9e308e047cc743b4da087e9b44a3

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include
+    $(call project-path-for,recovery)/updater/include \
+    $(call project-path-for,recovery)/edify/include \
+    $(call project-path-for,recovery)/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_g2
 LOCAL_MODULE_TAGS := eng
