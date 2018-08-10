f7d9b1ee1ef7e86368e869f6b9d402bbb7330983
Bruno Martins <bgcngm@gmail.com>
2018-08-10 23:14:57 +0100

    g2-common: Update Dalvik heap overrides location
     * These will live in vendor/lineage from now on
    
    Change-Id: Ifac6650a874cf982a6e767da92599d4b6230a637

--- g2.mk
+++ g2.mk
@@ -61,7 +61,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, vendor/lineage/config/dalvik/phone-xxhdpi-2048-dalvik-heap.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
