8ad844080c8c30dbdb1552172396feb2f873e727
Ahrotahn <BlaiseD@Gmail.com>
2018-08-14 15:49:00 +0200

    g2-common: hwaddrs: Create and validate addresses
    Validates the existing address files if they exist.  If not valid, try
    reading the addresses from /misc.  If that address isn't valid, then
    generate a random address.  Store a copy of the files in
    /persist for persistance.
    
    Change-Id: I111d7c90620ea601ef0f4665a9ed7de3e43637b7

--- hwaddrs/getmac.c
+++ hwaddrs/getmac.c
@@ -14,33 +14,105 @@
  * limitations under the License.
  */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <ctype.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-/* Read plain address from NV and set the BT MAC
- * address accordingly */
+/* Read plain address from NV, if invalid generate random.
+ * Set the BT MAC address accordingly */
+
+// Validates the contents of the given file
+int checkAddr(char* filepath) {
+    char charbuf[17];
+    int i, notallzeroes = 0, ret = 0;
+    int checkfd = open(filepath, O_RDONLY);
+
+    if (checkfd < 0) return 0; // doesn't exist/error
+
+    do {
+        if (read(checkfd, charbuf, 17) != 17) break;
+        for (i = 0; i < 17; i++) {
+            if (i % 3 != 2) {
+                if (!isxdigit(charbuf[i])) break;
+                if (charbuf[i] != '0') notallzeroes = 1;
+            } else if (charbuf[i] != ':') break;
+        }
+        ret = notallzeroes;
+    } while (0);
+
+    close(checkfd);
+    return ret;
+}
+
+// Writes a file using an address from the misc partition
+// Generates a random address if the one read contains only zeroes
+void writeAddr(char* filepath, int offset) {
+    uint8_t macbytes[6];
+    char macbuf[19];
+    int i, macnums = 0;
+    int miscfd = open("/dev/block/platform/msm_sdcc.1/by-name/misc", O_RDONLY);
+    int writefd = open(filepath, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
+    lseek(miscfd, offset, SEEK_SET);
+
+    for (i = 0; i < 6; i++) {
+        read(miscfd, &macbytes[i], 1);
+        macnums |= macbytes[i];
+    }
+
+    close(miscfd);
+
+    if (macnums == 0) {
+        // Generate random address, oreo hal style
+        macbytes[0] = 0x22;
+        macbytes[1] = 0x22;
+        macbytes[2] = (uint8_t) rand() % 256;
+        macbytes[3] = (uint8_t) rand() % 256;
+        macbytes[4] = (uint8_t) rand() % 256;
+        macbytes[5] = (uint8_t) rand() % 256;
+    }
+
+    sprintf(macbuf, "%02x:%02x:%02x:%02x:%02x:%02x\n",
+            macbytes[0], macbytes[1], macbytes[2], macbytes[3], macbytes[4], macbytes[5]);
+    write(writefd, &macbuf, 18);
+    close(writefd);
+}
+
+// Simple file copy
+void copyAddr(char* source, char* dest) {
+    char buffer;
+    int sourcefd = open(source, O_RDONLY);
+    int destfd = open(dest, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
+    if (sourcefd < 0 || destfd < 0) return; // doesn't exist/error
+
+    while (read(sourcefd, &buffer, 1) != 0) {
+        write(destfd, &buffer, 1);
+    }
+
+    close(sourcefd);
+    close(destfd);
+}
 
 int main() {
-	int fd1, fd2;
-	char macbyte;
-	char macbuf[3];
-	int i;
+    char *datavendorpath, *persistpath;
+    srand(time(NULL));
 
-	fd1 = open("/dev/block/platform/msm_sdcc.1/by-name/misc",O_RDONLY);
+    datavendorpath = "/data/misc/bluetooth/bdaddr";
+    persistpath = "/persist/.bdaddr";
+    if (checkAddr(datavendorpath) == 0) {
+        if (checkAddr(persistpath) == 0) {
+            writeAddr(persistpath, 0x4000);
+        }
+        copyAddr(persistpath, datavendorpath);
+    }
 
-	fd2 = open("/data/misc/bdaddr",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-	for (i = 0; i<6; i++) {
-		lseek(fd1,0x4000+i,SEEK_SET);
-		lseek(fd2,0,SEEK_END);
-		read(fd1,&macbyte,1);
-		sprintf(macbuf,"%02x",macbyte);
-		write(fd2,&macbuf,2);
-		if (i!=5) write(fd2,":",1);
-	}
-	close(fd2);
-
-	close(fd1);
-	return 0;
+    return 0;
 }
