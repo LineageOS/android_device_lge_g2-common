56aa8c4c4f3ec1762f88acd9f14679a17a74f98e
Bruno Martins <bgcngm@gmail.com>
2018-09-05 23:01:52 +0100

    g2-common: Only include legacy QC sepolicy
     * This has now turned into a separate repository (maintained only
       for legacy devices, those that never got official Oreo updates).
    
    Change-Id: I981b452b697bc3610d7aa97b74ed182c6b70ca30

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,8 +139,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USE_SDCLANG := true
 
 # SELinux policies
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/lge/g2-common/sepolicy
