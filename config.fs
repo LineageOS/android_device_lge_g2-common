b73fcacaaa32668d8fc3599c1f42c92e4b4e5803
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    g2-common: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Furthermore, with System As Root / is a read-only filesystem
       so we can't create our mountpoints from init script anymore
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- config.fs
+++ config.fs
@@ -6,3 +6,33 @@ value:2951
 
 [AID_VENDOR_RFS_SHARED]
 value:2952
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[mpt/]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist-lg/]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[sns/]
+mode: 0775
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
