45bb669933fea250087ce882e0c28db66737bec9
Alessandro Astone <ales.astone@gmail.com>
2019-09-10 09:57:50 +0200

    g2-common: add libbinder shim library
     * Commit f1cf11f1a4d5255554b49c6b299070676e53b72f of
       frameworks/native removed these support symbols
    
    Change-Id: I4fadd98c1ad11424b6ba5ff309d1e86dcf3cdca1

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -34,3 +34,13 @@ LOCAL_PROPRIETARY_MODULE := true
 LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := libbinder_interface.cpp
+LOCAL_MODULE := libshim_binder
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SHARED_LIBRARIES := libbinder libutils
+
+include $(BUILD_SHARED_LIBRARY)
