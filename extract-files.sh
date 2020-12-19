ffded7803819ade8f94218670ac91d0ea6a39e7c
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    g2-common: Switch to standalone extract utils
    Somewhat align with templates while we are at it.
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) $YEAR The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,15 +11,13 @@ set -e
 DEVICE_COMMON=g2-common
 VENDOR=lge
 
-INITIAL_COPYRIGHT_YEAR=2013
-
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="$MY_DIR/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$ANDROID_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -58,23 +46,27 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/permissions/qcnvitems.xml)
+            ;&
+        vendor/etc/permissions/qcrilhook.xml)
+            sed -i 's|/system/framework|/vendor/framework|g' "${2}"
+            ;;
+        vendor/bin/thermal-engine)
+            sed -i 's|/system/etc|/vendor/etc|g' "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true $clean_vendor
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ANDROID_ROOT" true $clean_vendor
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 # Initialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false $clean_vendor
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT" false $clean_vendor
 
 extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
-
-QCNVITEMS="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/etc/permissions/qcnvitems.xml
-sed -i 's|/system/framework|/vendor/framework|g' "$QCNVITEMS"
-
-QCRILHOOK="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/etc/permissions/qcrilhook.xml
-sed -i 's|/system/framework|/vendor/framework|g' "$QCRILHOOK"
-
-THERMAL_ENGINE="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/bin/thermal-engine
-sed -i 's|/system/etc|/vendor/etc|g' "$THERMAL_ENGINE"
 
 "$MY_DIR"/setup-makefiles.sh
