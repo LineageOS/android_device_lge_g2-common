eca4e28c5b32ec2799bfa423caa9c1037aba7947
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    g2-common: Switch to standalone extract utils
    Somewhat align with templates while we are at it.
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) $YEAR The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,15 +11,13 @@ set -e
 DEVICE_COMMON=g2-common
 VENDOR=lge
 
-INITIAL_COPYRIGHT_YEAR=2013
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="$MY_DIR/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$ANDROID_ROOT/tools/extract-utils/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,7 +25,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ANDROID_ROOT" true
 
 # Copyright headers and common guards
 write_headers "g2 d800 d801 d802 d803 f320 l01f ls980 vs980"
@@ -49,7 +37,7 @@ write_makefiles "$MY_DIR"/proprietary-files.txt true
 write_footers
 
 # Initialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
 
 # Copyright headers and guards
 write_headers
